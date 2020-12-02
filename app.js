2e1868f6d3c9c20302709bf8ec709d90526cfe45
Kshitiz Goel <57189655+kshitiz-goel07@users.noreply.github.com>
2020-12-02 15:26:48 +0530

    learning console

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-alert('hello Sir !! ');
+console.log("hello");
+console.log(123);
+var a="hi dear";
+console.log(a);
+console.log([1,2,3]);
+console.log({a:1,b:2});
+console.table([1,,4,7])
+
+console.error("error hai");
+console.warn('warning hai');
+console.time();
+console.log("hello");
+console.log(123);console.log("hello");
+console.log(123);console.log("hello");
+console.log(123);console.log("hello");
+console.log(123);console.log("hello");
+console.log(123);console.log("hello");
+console.log(123);console.log("hello");
+console.log(123);console.log("hello");
+console.log(123);console.log("hello");
+console.log(123);console.log("hello");
+console.log(123);console.log("hello");
+console.log(123);console.log("hello");
+console.log(123);console.log("hello");
+console.log(123);console.log("hello");
+console.log(123);
+console.timeEnd();
