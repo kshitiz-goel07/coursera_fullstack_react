71b894106347f329b209499b8e7b3ec13ae9cf50
Kshitiz Goel <57189655+kshitiz-goel07@users.noreply.github.com>
2020-12-12 11:37:28 +0530

    array methods

--- app.js
+++ app.js
@@ -56,6 +56,11 @@ let val;
 // val = Array.isArray(number);
 // val = number[1];
 // number[1]=10;
+// val = number.indexOf(6);
 
+// val = number.push(10);
+// val = number.unshift(21);
+// number.splice(1,3);
+number.reverse();
 console.log(number);
 console.log(val);
