0e3e29c27d8059dc7c42fd67158a4d686b8157cd
Kshitiz Goel <57189655+kshitiz-goel07@users.noreply.github.com>
2020-12-12 10:09:02 +0530

    learning array

--- app.js
+++ app.js
@@ -23,26 +23,39 @@
 
 // // console.log(val);
 
-const name = 'kshitiz goel';
-const age = '20';
-const job = 'web developer';
-const city = 'sre';
+// const name = 'kshitiz goel';
+// const age = '20';
+// const job = 'web developer';
+// const city = 'sre';
 
-let html;
-function hello(){
-    return 'hi there !!' ;
-}
-//with template strings
-html =
-`
-<ul>
-<li>Name : ${name}</li>
-<li>Age : ${age}</li>
-<li>Job : ${job}</li>
-<li>City : ${city}</li>
-<li>${2+2}</li>
-<li>${hello()}</li>
-<li>${age >18 ? 'you\'re big' : 'Go to home boy'}</li>
-</ul>
-`
-document.body.innerHTML = html;
+// let html;
+// function hello(){
+//     return 'hi there !!' ;
+// }
+// //with template strings
+// html =
+// `
+// <ul>
+// <li>Name : ${name}</li>
+// <li>Age : ${age}</li>
+// <li>Job : ${job}</li>
+// <li>City : ${city}</li>
+// <li>${2+2}</li>
+// <li>${hello()}</li>
+// <li>${age >18 ? 'you\'re big' : 'Go to home boy'}</li>
+// </ul>
+// `
+// document.body.innerHTML = html;
+
+const number = [2 , 5 , 1 , 3 , 6, 4];
+const numbers = new Array(11, 15 , 13 , 12 , 16, 14);
+const fruits = ['apple ', 'banana', 'guwava','kiwi'];
+
+let val;
+
+// val = Array.isArray(number);
+// val = number[1];
+// number[1]=10;
+
+console.log(number);
+console.log(val);
