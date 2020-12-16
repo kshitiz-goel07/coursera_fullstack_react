6d331d61592202daac354baccfdea250e3b26a0d
Kshitiz Goel <57189655+kshitiz-goel07@users.noreply.github.com>
2020-12-17 01:36:57 +0530

    learned if/else

--- app.js
+++ app.js
@@ -1,25 +1,7 @@
-// const person = {
-//     firstName : 'Kshitiz',
-//     lastName : 'Goel',
-//      age : 20,
-//      hobbies : ['cricket', 'games','guitar'],
-//      address : {
-//          city : 'Saharanpur',
-//          state : 'UP'
-//      },
-//      getBirthYear : function(){
-//          return 2000;
-//      }
-// }
-// let val;
-
-// val = person;
-// val = person.firstName;
-// val = person['lastName'];
-// val = person.getBirthYear();
-// console.log(val);
-let val;
-const today = new Date();
-let birthday = new Date('7-17-2000')
-console.log(birthday);
-console.log(today);
+const id = '100';
+if (id === 100){
+    console.log('CORRECT');
+}
+else{
+    console.log('INCORRECT');
+}
