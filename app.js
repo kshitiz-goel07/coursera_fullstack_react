f571846d0c47dd865e948c74faf959b0bb90cfed
Kshitiz Goel <57189655+kshitiz-goel07@users.noreply.github.com>
2020-12-16 23:44:42 +0530

    learned object litreals

--- app.js
+++ app.js
@@ -1,66 +1,20 @@
-// const firstName = 'kshitiZs';
-// const lastName = 'GoeL' ;
-
-
-// let val;
-// val  = firstName  + ' ' + lastName ;
-// val = 'rajenDRA';
-// val += ' GUPta';
-
-// val = val.concat(' ', ',Hi ');
-// val = firstName.toLowerCase();
-// val = firstName.toUpperCase();
-// val = lastName.length;
-// val = "hi , that's my hobbie";
-// val = 'hi , friend\'s'
-
-// val = firstName.charAt(2);
-// val = firstName.indexOf('s');
-// val = firstName.lastIndexOf('s');
-
-// val = lastName.slice(0,2);
-// val = lastName.slice(-2);
-
-// // console.log(val);
-
-// const name = 'kshitiz goel';
-// const age = '20';
-// const job = 'web developer';
-// const city = 'sre';
-
-// let html;
-// function hello(){
-//     return 'hi there !!' ;
-// }
-// //with template strings
-// html =
-// `
-// <ul>
-// <li>Name : ${name}</li>
-// <li>Age : ${age}</li>
-// <li>Job : ${job}</li>
-// <li>City : ${city}</li>
-// <li>${2+2}</li>
-// <li>${hello()}</li>
-// <li>${age >18 ? 'you\'re big' : 'Go to home boy'}</li>
-// </ul>
-// `
-// document.body.innerHTML = html;
-
-const number = [2 , 5 , 1 , 3 , 6, 4];
-const numbers = new Array(11, 15 , 13 , 12 , 16, 14);
-const fruits = ['apple ', 'banana', 'guwava','kiwi'];
-
+const person = {
+    firstName : 'Kshitiz',
+    lastName : 'Goel',
+     age : 20,
+     hobbies : ['cricket', 'games','guitar'],
+     address : {
+         city : 'Saharanpur',
+         state : 'UP'
+     },
+     getBirthYear : function(){
+         return 2000;
+     }
+}
 let val;
 
-// val = Array.isArray(number);
-// val = number[1];
-// number[1]=10;
-// val = number.indexOf(6);
-
-// val = number.push(10);
-// val = number.unshift(21);
-// number.splice(1,3);
-number.reverse();
-console.log(number);
+val = person;
+val = person.firstName;
+val = person['lastName'];
+val = person.getBirthYear();
 console.log(val);
