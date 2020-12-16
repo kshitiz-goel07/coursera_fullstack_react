2bcf597030279aa22d8a9a5b94493e0c9b82f469
Kshitiz Goel <57189655+kshitiz-goel07@users.noreply.github.com>
2020-12-17 00:39:25 +0530

    learned dates and time function

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
-const person = {
-    firstName : 'Kshitiz',
-    lastName : 'Goel',
-     age : 20,
-     hobbies : ['cricket', 'games','guitar'],
-     address : {
-         city : 'Saharanpur',
-         state : 'UP'
-     },
-     getBirthYear : function(){
-         return 2000;
-     }
-}
-let val;
+// const person = {
+//     firstName : 'Kshitiz',
+//     lastName : 'Goel',
+//      age : 20,
+//      hobbies : ['cricket', 'games','guitar'],
+//      address : {
+//          city : 'Saharanpur',
+//          state : 'UP'
+//      },
+//      getBirthYear : function(){
+//          return 2000;
+//      }
+// }
+// let val;
 
-val = person;
-val = person.firstName;
-val = person['lastName'];
-val = person.getBirthYear();
-console.log(val);
+// val = person;
+// val = person.firstName;
+// val = person['lastName'];
+// val = person.getBirthYear();
+// console.log(val);
+let val;
+const today = new Date();
+let birthday = new Date('7-17-2000')
+console.log(birthday);
+console.log(today);
