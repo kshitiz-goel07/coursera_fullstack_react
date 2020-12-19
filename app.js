8ff6231c419775db4295cd2ad2d0945cab5b69c9
Kshitiz Goel <57189655+kshitiz-goel07@users.noreply.github.com>
2020-12-19 21:12:55 +0530

    learned switch case

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
-const id = '100';
-if (id === 100){
-    console.log('CORRECT');
+const color = 'red';
+switch(color){
+   case 'red' : console.log('color is red');
+   break;
+   case 'yellow' : console.log('color is yellow');
+   break;
+   default : console.log('color isn\'t red nor yellow ' );
+   break;
 }
-else{
-    console.log('INCORRECT');
+
+let  day;
+day = new Date().getDay();
+
+switch(day){
+    case 0 : console.log('mon');
+    break;
+    case 1 : console.log('t');
+    break;
+    case 2 : console.log('w');
+    break;
+    case 3 : console.log('thur');
+    break;
+    case 4 : console.log('fri');
+    break;
+    case 5 : console.log('sat');
+    break;
+    case 6 : console.log('sun');
+    break;
 }
