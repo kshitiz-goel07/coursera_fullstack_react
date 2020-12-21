677068781253f02e9bce5b490e980ed4eced6847
Kshitiz Goel <57189655+kshitiz-goel07@users.noreply.github.com>
2020-12-21 06:52:59 +0530

    learning function in js

--- app.js
+++ app.js
@@ -1,29 +1,8 @@
-const color = 'red';
-switch(color){
-   case 'red' : console.log('color is red');
-   break;
-   case 'yellow' : console.log('color is yellow');
-   break;
-   default : console.log('color isn\'t red nor yellow ' );
-   break;
+// FUNCTION DECLERATION
+
+function greet(){
+    console.log('hello');
+
 }
 
-let  day;
-day = new Date().getDay();
-
-switch(day){
-    case 0 : console.log('mon');
-    break;
-    case 1 : console.log('t');
-    break;
-    case 2 : console.log('w');
-    break;
-    case 3 : console.log('thur');
-    break;
-    case 4 : console.log('fri');
-    break;
-    case 5 : console.log('sat');
-    break;
-    case 6 : console.log('sun');
-    break;
-}
+greet();
