a6df319110b16e2106e021c5cf892e45c654cab3
Kshitiz Goel <57189655+kshitiz-goel07@users.noreply.github.com>
2021-01-03 21:33:05 +0530

    While loop

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-//  FOR LOOP 
-for(let i = 0 ; i < 10 ; i++)
+// //  FOR LOOP 
+// for(let i = 0 ; i < 10 ; i++)
+// {
+//     if (i === 3)
+//     {
+//         console.log('number 3 is my fav');
+//         continue;
+//     }
+
+//     if(i === 6)
+//     {
+//         console.log('Time to stop the loop');
+//         break;
+//     }
+//     console.log('number is '+ i);
+
+//  }
+
+let i=0;
+while( i < 10 )
 {
-    if (i === 3)
-    {
-        console.log('number 3 is my fav');
-        continue;
-    }
-
-    if(i === 6)
-    {
-        console.log('Time to stop the loop');
-        break;
-    }
     console.log('number is '+ i);
-
+    i++;
 }
